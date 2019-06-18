70ab3bfdcb972a8eac7b9a74c0db43e1958c22a7
MADRZYK FRANCIS <FRA9974652@office.adforest.lan>
2019-06-18 16:48:15 +0200

    :tada: List producs and add products

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
+<!-- src/components/ProductList.vue -->
 <template>
-  <!-- when using a component in a template Vue.js expects the DOM element to be
-  lowercased and dasherized. e.g. ProductList -> product-list -->
-  <div class="welcome">Hello World !</div>
-  <!-- use the <product-list> component here! -->
+    <manage-products/>
 </template>
 
 <script>
-  // import product list component
-  export default {
-    name: 'App',
-    components: {
-      // add product list as a component
-    }
+import ManageProducts from "./components/ManageProducts";
+export default {
+  components: {
+    ManageProducts,
   }
+}
 </script>
