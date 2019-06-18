c91a9689a9a4893d4a3ae403c52b9c14aff4c763
MADRZYK FRANCIS <FRA9974652@office.adforest.lan>
2019-06-18 11:21:39 +0200

    :tada: Init project with bootstrap

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- when using a component in a template Vue.js expects the DOM element to be
+  lowercased and dasherized. e.g. ProductList -> product-list -->
+  <div class="welcome">Hello World !</div>
+  <!-- use the <product-list> component here! -->
+</template>
+
+<script>
+  // import product list component
+  export default {
+    name: 'App',
+    components: {
+      // add product list as a component
+    }
+  }
+</script>
